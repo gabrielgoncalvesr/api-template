ccc10f5cb7ffc3bef1efc9370a9c8f2670363baf
Gabriel Gonçalves Reis <greis@spo.office.br.lyra>
2020-06-07 13:18:14 -0300

    fixing wrong env variable

--- src/server.js
+++ src/server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 const message = `Application running in ${process.env.NODE_ENV} mode. Initing server on port ${process.env.API_PORT}`
 
-app.listen(process.env.POAPI_PORTRT, () => console.log(message));
+app.listen(process.env.API_PORT, () => console.log(message));
